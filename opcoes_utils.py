27b63062c6d8869cb6dd47cd0d9ceb7e6bceb3b5
João Guilherme <joaogojunior@gmail.com>
2023-06-18 17:54:41 -0300

    chaves removidas, .gitignore atualizado, novas opcoes padroes e resultado.csv movido

--- opcoes_utils.py
+++ opcoes_utils.py
@@ -1,7 +1,10 @@
 import json
 
-opcoes_dict = json.loads("./config/opcoes.json")
+opcoes_file = open("./config/opcoes.json", "r")
+opcoes_dict = json.load(opcoes_file)
+
+print(opcoes_dict)
 
 
 def le_opcao(chave):
-    return opcoes_dict["chave"]
+    return opcoes_dict[chave]
